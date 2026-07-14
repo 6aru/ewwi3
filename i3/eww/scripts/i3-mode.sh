2c476a5a10ed0657373a74adc2a7786d620bff2c
Baru <216474718+6aru@users.noreply.github.com>
2026-07-14 12:38:52 +0530

    Fix script path in i3-mode.sh
    Update the script path for i3-mode.sh.

--- i3/eww/scripts/i3-mode.sh
+++ i3/eww/scripts/i3-mode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Simpan sebagai ~/.config/eww/scripts/i3-mode.sh
+# Simpan sebagai ~/.config/i3/eww/scripts/i3-mode.sh
 
 i3-msg -t subscribe -m '["mode"]' | while read -r line; do
     # Parse JSON untuk mendapatkan nama mode

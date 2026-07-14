6a86a643277e5efdf2a05b1098595d50ef5d4d0d
Baru <216474718+6aru@users.noreply.github.com>
2026-07-14 12:45:54 +0530

    Fix script path for filter-apps.sh

--- i3/eww/scripts/switch-category.sh
+++ i3/eww/scripts/switch-category.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CATEGORY="${1:-Internet}"
-APPS=$(~/.config/eww/scripts/filter-apps.sh "$CATEGORY")
+APPS=$(~/.config/i3/eww/scripts/filter-apps.sh "$CATEGORY")
 eww update menu_active_cat="$CATEGORY"
 eww update active_apps="$APPS"
